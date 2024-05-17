330d83670f312b3ba6b0173917cbe2df8b5db7eb
FionaGreenland2023 <chawsuhlaing.cs65@gmail.com>
2024-05-17 13:00:49 +0630

    backup

---
+++ project/Todo/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans:[
+        "Montserrat" , "Padauk" , "sans-serif"
+      ]
+    }
+,
+    extend: {
+      fontFamily: {
+        heading : [ "Merriweather", "serif"]
+      }
+    },
+  },
+  plugins: [],
+}
